bd05ce660b966fdeb05e5d24635f7733b9238787
Conor Luppnow <conorluppnow@gmail.com>
2020-12-07 01:13:27 -0500

    able to plot raoult and wilson

--- proj2A.m
+++ proj2A.m
@@ -1,40 +1,156 @@
-% Raoult's Law
-% y_i*P = x_i * P_i^sat
 
-% Using antoine eqn for P_methanol sat
 clc; clear;
 P = 42.66; %kPa
 P_bar = 0.4266;
 
-% For methanol: A = 11.9673 B = 3626.55 C = -34.29
-% For water: A = 11.6834 B = 3816.44 C = -46.13
+R = 8.314;
 
+%%
+x1 = 0:0.05:1;
+x2 = 1 - x1;
 
-
-syms x
+T_init = 350;
+epsilon = 0.0001;
 
 A_met = 11.9673; B_met = 3626.55; C_met = -34.29;
 A_water = 11.6834; B_water = 3816.44; C_water = -46.13;
-x1 = 0:0.05:1;
-x2 = 1-x1;
 
-for i=1:numel(x1)
+
+
+
+%% Raoult Law
+for i = 1:numel(x1)
+    y_tot_R(i) = 2;
+    T_R(i) = T_init;
+    j=1;
+    while abs(y_tot_R(i)-1) > epsilon
+            
+            while j==1
+            P1_sat_R(i) = exp(A_met - B_met/(T_R(i) + C_met));
+            P2_sat_R(i) = exp(A_water - B_water/(T_R(i) + C_water));
+
+            K1_R(i) = P1_sat_R(i)/P_bar;
+            K2_R(i) = P2_sat_R(i)/P_bar;
+            
+            y_tot_R(i) = K1_R(i) * x1(i) + K2_R(i) * x2(i);
+            j = j+1;
+            end
+            
+        
+        if y_tot_R(i) > 1
+            T_R(i) = 0.9999 * T_R(i);
+            
+            P1_sat_R(i) = exp(A_met - B_met/(T_R(i) + C_met));
+            P2_sat_R(i) = exp(A_water - B_water/(T_R(i) + C_water));
+
+            K1_R(i) = P1_sat_R(i)/P_bar;
+            K2_R(i) = P2_sat_R(i)/P_bar;
+            
+            y_tot_R(i) = K1_R(i) * x1(i) + K2_R(i) * x2(i);
+            
+        end
+        
+        if y_tot_R(i) < 1
+            T_R(i) = 1.0001 * T_R(i);
+            
+            P1_sat_R(i) = exp(A_met - B_met/(T_R(i) + C_met));
+            P2_sat_R(i) = exp(A_water - B_water/(T_R(i) + C_water));
+
+            K1_R(i) = P1_sat_R(i)/P_bar;
+            K2_R(i) = P2_sat_R(i)/P_bar;
+            
+            y_tot_R(i) = K1_R(i) * x1(i) + K2_R(i) * x2(i);
+            
+        end
+        
+
+    end
+    y1_R(i) = K1_R(i) * x1(i);
     
-    eqn = (x1(i)*exp(A_met - B_met/(x + C_met))/P_bar) + (x2(i)*exp(A_water - B_water/(x + C_water))/P_bar) == 1;
-    T(i) = solve(eqn,x);
 end
 
+plot(x1,T_R,'--b',y1_R,T_R,'--r'); hold on;
 
-for i=1:numel(x1)
-    
-    P1_sat(i) = exp(A_met - B_met/(T(i) + C_met))/P_bar;
-    P2_sat(i) = exp(A_water - B_water/(T(i) + C_water))/P_bar;
-    
-    y1(i) = x1(i) * P1_sat(i) / P_bar;
-    y2(i) = x2(i) * P2_sat(i) / P_bar;
+%% Wilson
+
+% Using values given for lamdba
+
+%lambda_12 = -1.4636e+03 and lambda_21 = 2.8397e+03;
+
+l_12 = -1.4636e+03; l_21 = 2.8397e+03;
+v1 = 41.489; v2 = 18.156;
+
+
+
+for k = 1:numel(x1)
+    y_tot_W(k) = 2;
+    T_W(k) = T_init;
+    q=1;
+    while abs(y_tot_W(k)-1) > epsilon
+            
+            while q==1
+            L_12(k) = (v2/v1) * exp(-l_12/(R*T_W(k)));
+            L_21(k) = (v1/v2) * exp(-l_21/(R*T_W(k)));
+            Omega(k) = (L_12(k)/(x1(k)+x2(k)*L_12(k))) - (L_21(k)/(x2(k)+x1(k)*L_21(k)));
+            
+            gamma_1(k) = exp(-log(x1(k) + x2(k)*L_12(k)) + x2(k)*Omega(k));
+            gamma_2(k) = exp(-log(x2(k) + x1(k)*L_21(k)) - x1(k)*Omega(k));
+            
+            P1_sat_W(k) = exp(A_met - B_met/(T_W(k) + C_met));
+            P2_sat_W(k) = exp(A_water - B_water/(T_W(k) + C_water));
+
+            K1_W(k) = P1_sat_W(k)*gamma_1(k)/P_bar;
+            K2_W(k) = P2_sat_W(k)*gamma_2(k)/P_bar;
+            
+            y_tot_W(k) = K1_W(k) * x1(k) + K2_W(k) * x2(k);
+            q = q+1;
+            end
+            
+        
+        if y_tot_W(k) > 1
+            T_W(k) = 0.9999 * T_W(k);
+            
+            L_12(k) = (v2/v1) * exp(-l_12/(R*T_W(k)));
+            L_21(k) = (v1/v2) * exp(-l_21/(R*T_W(k)));
+            Omega(k) = (L_12(k)/(x1(k)+x2(k)*L_12(k))) - (L_21(k)/(x2(k)+x1(k)*L_21(k)));
+            
+            gamma_1(k) = exp(-log(x1(k) + x2(k)*L_12(k)) + x2(k)*Omega(k));
+            gamma_2(k) = exp(-log(x2(k) + x1(k)*L_21(k)) - x1(k)*Omega(k));
+            
+            P1_sat_W(k) = exp(A_met - B_met/(T_W(k) + C_met));
+            P2_sat_W(k) = exp(A_water - B_water/(T_W(k) + C_water));
+
+            K1_W(k) = P1_sat_W(k)*gamma_1(k)/P_bar;
+            K2_W(k) = P2_sat_W(k)*gamma_2(k)/P_bar;
+            
+            y_tot_W(k) = K1_W(k) * x1(k) + K2_W(k) * x2(k);
+            
+        end
+        
+        if y_tot_W(k) < 1
+            T_W(k) = 1.0001 * T_W(k);
+            
+            L_12(k) = (v2/v1) * exp(-l_12/(R*T_W(k)));
+            L_21(k) = (v1/v2) * exp(-l_21/(R*T_W(k)));
+            Omega(k) = (L_12(k)/(x1(k)+x2(k)*L_12(k))) - (L_21(k)/(x2(k)+x1(k)*L_21(k)));
+            
+            gamma_1(k) = exp(-log(x1(k) + x2(k)*L_12(k)) + x2(k)*Omega(k));
+            gamma_2(k) = exp(-log(x2(k) + x1(k)*L_21(k)) - x1(k)*Omega(k));
+            
+            P1_sat_W(k) = exp(A_met - B_met/(T_W(k) + C_met));
+            P2_sat_W(k) = exp(A_water - B_water/(T_W(k) + C_water));
+
+            K1_W(k) = P1_sat_W(k)*gamma_1(k)/P_bar;
+            K2_W(k) = P2_sat_W(k)*gamma_2(k)/P_bar;
+            
+            y_tot_W(k) = K1_W(k) * x1(k) + K2_W(k) * x2(k);
+            
+        end
+        
+
+    end
+    y1_W(k) = K1_W(k) * x1(k);
     
 end
 
-plot(x1,T,'ob',y1,T,'or');
-grid on;
-
+plot(x1,T_W,'-b',y1_W,T_W,'-r'); hold on; grid on;
